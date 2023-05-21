16a9f62c49bcc48b13b902bbb14a1bcb5391f2e1
eshmambetolon <eshmambetolon@gmail.com>
2023-05-21 15:44:02 +0600

    template changes

--- search/views.py
+++ search/views.py
@@ -1,9 +1,21 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from person.models import Victim
 
 def search(request):
     query = request.GET.get('q')
     results = None
+    paginator = Paginator([], 2)
+
     if query:
         results = Victim.objects.filter(content__icontains=query)
-    return render(request, 'search_page.html', {'results': results})
+        paginator = Paginator(results, 2)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "page_obj": page_obj,
+        "results": results,
+    }
+    return render(request, 'search_page.html', context)
